22215182c51ada7f29a51cf3cf67ef6a58b8f274
simon_blamo <sblamo@ets.org>
2024-11-03 00:22:22 -0400

    Made hangman class, revamped hangman CLI, made bare-bones working pyqt App.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from PyQt6.QtCore import *
 from layout_colorwidget import Color
-from hangman import *
+from hangman import Hangman
 import sys
 
 # yollooooooooooooooooooooooooooooooooooooooooo
@@ -11,77 +11,156 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Hangman")
-        keyboard_row_1_chars = 'qwertyuiop'
-        keyboard_row_2_chars = 'asdfghjkl'
-        keyboard_row_3_chars = 'zxcvbnm'
+        self.hangman_game: Hangman = Hangman()
+        self.easy_btn: QPushButton = None
+        self.medium_btn: QPushButton = None
+        self.hard_btn: QPushButton = None
+        self.guess_text_box: QLineEdit = None
+        self.keyboard_btns: List[List[QPushButton]] = None
+        self.guess_btn: QPushButton = None
+        self.game_progress_boxes: List[QLineEdit] = None
         
 
-        pagelayout = QVBoxLayout()
-        button_layout = QHBoxLayout()
+        page_layout = QVBoxLayout()
+        difficulty_btn_layout = QHBoxLayout()
+        self.game_progress_layout = QHBoxLayout()
         image_layout = QHBoxLayout()
         input_layout = QHBoxLayout()
-        keyboard_layout = QVBoxLayout()
         keyboard_container_layout = QVBoxLayout()
-        keyboard_row_1_layout = QHBoxLayout()
-        keyboard_row_2_layout = QHBoxLayout()
-        keyboard_row_3_layout = QHBoxLayout()
+
+        keyboard_widget = None
+        
         self.stacklayout = QStackedLayout()
 
-        pagelayout.addLayout(button_layout)
-        pagelayout.addLayout(image_layout)
-        pagelayout.addLayout(input_layout)
-        pagelayout.addLayout(keyboard_container_layout)
-        pagelayout.addLayout(self.stacklayout)
+        page_layout.addLayout(difficulty_btn_layout)
+        page_layout.addLayout(image_layout)
+        page_layout.addLayout(self.game_progress_layout)
+        page_layout.addLayout(input_layout)
+        page_layout.addLayout(keyboard_container_layout)
+        page_layout.addLayout(self.stacklayout)
 
+        self.init_difficulty_btns(difficulty_btn_layout)
+        self.init_hangman_image(image_layout)
+
+        self.guess_text_box = self.init_guess_text_box()
+        input_layout.addWidget(self.guess_text_box)
+
+        keyboard_widget, self.keyboard_btns = self.init_keyboard_widget()
+        keyboard_container_layout.addWidget(keyboard_widget, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        ## Code that will be used a reference to achieve a future goal.
+        
+        # self.stacklayout.addWidget(Color("yellow"))
+
+        widget = QWidget()
+        widget.setLayout(page_layout)
+        self.setCentralWidget(widget)
+        self.set_tab_order()
+
+    ### METHODS TO INITIALIZE ELEMENTS WITHIN APP WINDOW ###
+
+    def init_difficulty_btns(self, difficulty_btn_layout):
+        btn = QPushButton("Easy")
+        btn.pressed.connect(lambda:self.start_game(0))
+        self.easy_btn = btn
+        difficulty_btn_layout.addWidget(btn)
+
+        btn = QPushButton("Medium")
+        btn.pressed.connect(lambda:self.start_game(1))
+        self.medium_btn = btn
+        difficulty_btn_layout.addWidget(btn)
+
+        btn = QPushButton("Hard")
+        btn.pressed.connect(lambda:self.start_game(2))
+        self.hard_btn = btn
+        difficulty_btn_layout.addWidget(btn)
+
+    def init_hangman_image(self, image_layout):
         label = QLabel(self)
         pixmap = QPixmap('./assests/stick.png').scaled(128, 192)
         label.setPixmap(pixmap)
         self.resize(pixmap.width(), pixmap.height())
         image_layout.addWidget(label, alignment=Qt.AlignmentFlag.AlignCenter)
 
-        edit = QLineEdit()
-        input_layout.addWidget(edit)
+    def init_guess_text_box(self):
+        text_box = QLineEdit()
+        text_box.setMaxLength(1)
+
+        font_metrics = text_box.fontMetrics()
+        width = font_metrics.horizontalAdvance('MM')
+
+        reg_ex = QRegularExpression('[A-Za-z]{1}')
+        validator = QRegularExpressionValidator(reg_ex)
+
+        text_box.setValidator(validator)
+        text_box.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        text_box.setFixedWidth(width + 10)
+
+        self.disable_textbox(text_box)
+        return text_box
+    
+    def init_keyboard_widget(self):
+        keyboard_layout = QVBoxLayout()
+        keyboard_row_1_layout = QHBoxLayout()
+        keyboard_row_2_layout = QHBoxLayout()
+        keyboard_row_3_layout = QHBoxLayout()
+        keyboard_row_4_layout = QHBoxLayout()
+        keyboard_widget = QWidget()
+
+        keyboard_row_1_chars = list('ABCDEFGHIJ')
+        keyboard_row_2_chars = list('KLMNOPQRS')
+        keyboard_row_3_chars = list('TUVWXYZ')
+        keyboard_row_3_chars.append('\u232B') # erase to left unicode
+        keyboard_row_4_word = 'GUESS'
+
+        keyboard_row_1_btns = []
+        keyboard_row_2_btns = []
+        keyboard_row_3_btns = []
+        keyboard_row_4_btns = []
+        btns_array = []
+        
 
         for char in keyboard_row_1_chars:
-            btn = QPushButton(char.upper())
+            btn = QPushButton(char)
+            keyboard_row_1_btns.append(btn)
+            btn.pressed.connect(lambda char=char:self.input_character_in_text_box(char, self.guess_text_box))
             keyboard_row_1_layout.addWidget(btn)
         
         for char in keyboard_row_2_chars:
-            btn = QPushButton(char.upper())
+            btn = QPushButton(char)
+            keyboard_row_2_btns.append(btn)
+            btn.pressed.connect(lambda char=char:self.input_character_in_text_box(char, self.guess_text_box))
             keyboard_row_2_layout.addWidget(btn)
 
         for char in keyboard_row_3_chars:
-            btn = QPushButton(char.upper())
+            btn = QPushButton(char)
+            keyboard_row_3_btns.append(btn)
+            btn.pressed.connect(lambda char=char:self.input_character_in_text_box(char, self.guess_text_box))
             keyboard_row_3_layout.addWidget(btn)
+
+        btn = QPushButton(keyboard_row_4_word)
+        btn.pressed.connect(lambda:self.process_guess(self.guess_text_box.text()))
+        keyboard_row_4_btns.append(btn)
+        keyboard_row_4_layout.addWidget(btn)
+        self.guess_btn = btn
+
+        btns_array.append(keyboard_row_1_btns)
+        btns_array.append(keyboard_row_2_btns)
+        btns_array.append(keyboard_row_3_btns)
+        btns_array.append(keyboard_row_4_btns)
+
+        self.disable_keyboard(btns_array)
 
         keyboard_layout.addLayout(keyboard_row_1_layout)
         keyboard_layout.addLayout(keyboard_row_2_layout)
         keyboard_layout.addLayout(keyboard_row_3_layout)
-        keyboard_widget = QWidget()
+        keyboard_layout.addLayout(keyboard_row_4_layout)
         keyboard_widget.setLayout(keyboard_layout)
         keyboard_widget.setFixedWidth(400)
-        keyboard_container_layout.addWidget(keyboard_widget, alignment=Qt.AlignmentFlag.AlignCenter)
 
-        ## Code that will be used a reference to achieve a future goal.
-        btn = QPushButton("Easy")
-        # btn.pressed.connect(self.activate_tab_1)
-        button_layout.addWidget(btn)
-        # self.stacklayout.addWidget(Color("red"))
+        return [keyboard_widget, btns_array]
 
-        btn = QPushButton("Medium")
-        # btn.pressed.connect(self.activate_tab_2)
-        button_layout.addWidget(btn)
-        # self.stacklayout.addWidget(Color("green"))
-
-        btn = QPushButton("Hard")
-        # btn.pressed.connect(self.activate_tab_3)
-        button_layout.addWidget(btn)
-        # self.stacklayout.addWidget(Color("yellow"))
-
-        widget = QWidget()
-        widget.setLayout(pagelayout)
-        self.setCentralWidget(widget)
-
+    ### END OF METHODS TO INITIALIZE ELEMENTS WITHIN APP WINDOW ###
     def activate_tab_1(self):
         self.stacklayout.setCurrentIndex(0)
 
@@ -90,7 +169,103 @@ class MainWindow(QMainWindow):
 
     def activate_tab_3(self):
         self.stacklayout.setCurrentIndex(2)
-       
+    
+    ### HELPER METHODS FOR ELEMENTS WITHIN APP WINDOW ###
+
+    ## text box
+    def disable_textbox(self, text_box):
+        text_box.setDisabled(True)
+
+    def enable_textbox(self, text_box):
+        text_box.setDisabled(False)
+    
+    ## keyboard
+    def disable_keyboard(self, keyboard_btns):
+        for keyboard_row in keyboard_btns:
+            for btn in keyboard_row:
+                btn.setDisabled(True)
+
+    def enable_keyboard(self, keyboard_btns):
+        for keyboard_row in keyboard_btns:
+            for btn in keyboard_row:
+                btn.setDisabled(False)
+
+    ## text box and keyboard
+    def input_character_in_text_box(self, char, text_box):
+        backspace = '\u232B'
+        if char != backspace:
+            text_box.setText(char)
+        else:
+            text_box.clear()
+    
+    ## tabbing order
+    def set_tab_order(self):
+        self.setTabOrder(self.easy_btn, self.medium_btn)
+        self.setTabOrder(self.medium_btn, self.hard_btn)
+        self.setTabOrder(self.hard_btn, self.guess_text_box)
+        self.setTabOrder(self.guess_text_box, self.guess_btn)
+    
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_Return or event.key() == Qt.Key.Key_Enter:
+            if self.focusWidget() == self.guess_text_box:
+                input_text = self.guess_text_box.text()
+                self.process_guess(input_text)
+            else:
+                self.focusWidget().click()
+
+        # Call the base class method to ensure normal event processing
+        super().keyPressEvent(event)
+
+    ### END OF HELPER METHODS FOR ELEMENTS WITHIN APP WINDOW ###
+
+    ### METHODS RELATED TO EXECUTION OF THE HANGMAN GAME ###
+
+    def start_game(self, difficulty):
+        self.hangman_game.reset_hangman()
+        self.hangman_game.set_current_word(difficulty)
+        print(self.hangman_game.get_current_word())
+        self.enable_keyboard(self.keyboard_btns)
+        self.enable_textbox(self.guess_text_box)
+        self.update_game_progress_widget(True)
+
+    def clear_layout(self, layout):
+        while layout.count():
+            item = layout.takeAt(0)
+            widget = item.widget()
+            if widget is not None:
+                widget.deleteLater()
+    
+    def update_game_progress_widget(self, new_game):
+        if self.hangman_game.current_word != None:
+            if new_game:
+                self.clear_layout(self.game_progress_layout)
+                self.game_progress_boxes = []
+                for char in self.hangman_game.current_word:
+                    text_box = QLineEdit()
+                    text_box.setMaxLength(1)
+
+                    font_metrics = text_box.fontMetrics()
+                    width = font_metrics.horizontalAdvance('MM')
+                    text_box.setFixedWidth(width + 10)
+
+                    self.disable_textbox(text_box)
+                    self.game_progress_boxes.append(text_box)
+                    self.game_progress_layout.addWidget(text_box)
+            else:
+                for index, progress_box in enumerate(self.game_progress_boxes):
+                    progress_box.setText(self.hangman_game.current_word_progress[index])
+    
+    def process_guess(self, input):
+        the_was_guess_correct = self.hangman_game.process_guess(input)
+        if the_was_guess_correct:
+            self.update_game_progress_widget(False)
+        if self.hangman_game.is_the_game_over:
+            self.disable_keyboard(self.keyboard_btns)
+            self.disable_textbox(self.guess_text_box)
+            print(self.hangman_game.did_you_win)
+
+    ### END OF METHODS RELATED TO EXECUTION OF THE HANGMAN GAME ###
+
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
--- hangman.py
+++ hangman.py
@@ -1,140 +1,147 @@
 import random
-import os
-import re
-def clear_terminal():
-  if os.name == 'nt':
-    os.system("cls")
-  else:
-    os.system("clear")
 
-def difficulty_prompt():
-    return_value = 0
-    user_choice_valid = False
-    user_choice = input("Welcome to Hangman! Select a difficulty to get started! \nYou have the options of EASY, MEDIUM, or HARD: ")
-    while user_choice_valid == False:
-        if(user_choice.lower() == 'exit'):
-            exit()
-        if user_choice.lower() == 'easy':
-            user_choice_valid = True
-            return_value = 0
-        elif user_choice.lower() == 'medium':
-            user_choice_valid = True
-            return_value = 1
-        elif user_choice.lower() == 'hard':
-            user_choice_valid = True
-            return_value = 2
-        else:
-            print()
-            print("Please enter a valid option.")
-            user_choice = input("You have the options of EASY, MEDIUM, or HARD: ")
+class Hangman():
+    def __init__(self):
+        self.number_of_wrong_guesses = 0
+        self.is_the_game_over = False
+        self.did_you_win = None
+        self.current_word = None
+        self.correct_char_guesses = []
+        self.incorrect_char_guesses = []
+        self.current_word_progress = []
+        self.chars_positions_in_word_dict = {}
+        
+    '''
+    3 difficulty levels.
+    0 = Easy
+    1 = Medium
+    2 = Hard
+    '''
+    def set_current_word(self, difficulty):
+        list_of_words = []
+        chosen_word = None
+        if difficulty == 0:
+            with open("easyWords.txt", "r") as f:
+                for line in f:
+                    list_of_words.append(line)
+            chosen_word = random.choice(list_of_words)
+        elif difficulty == 1:
+            with open("medWords.txt", "r") as f:
+                for line in f:
+                    list_of_words.append(line)
+            chosen_word = random.choice(list_of_words)
+        elif difficulty == 2:
+            with open("hardWords.txt", "r") as f:
+                for line in f:
+                    list_of_words.append(line)
+            chosen_word = random.choice(list_of_words)
+        self.current_word = chosen_word.upper().strip()
+        self.update_current_word_progress()
+        self.update_chars_positions_dict()
+
+    def get_current_word(self):
+        return self.current_word
     
-    return return_value
-
-'''
-3 difficulty levels.
-0 = Easy
-1 = Medium
-2 = Hard
-'''
-def select_word(difficulty):
-    return_word = None
-    list_of_words = []
-    if difficulty == 0:
-        with open("easyWords.txt", "r") as f:
-            for line in f:
-                list_of_words.append(line)
-        return_word = random.choice(list_of_words)
-    elif difficulty == 1:
-        with open("medWords.txt", "r") as f:
-            for line in f:
-                list_of_words.append(line)
-        return_word = random.choice(list_of_words)
-    elif difficulty == 2:
-        with open("hardWords.txt", "r") as f:
-            for line in f:
-                list_of_words.append(line)
-        return_word = random.choice(list_of_words)
-    return return_word.strip()
-
-def all_chars_found(correct_chars_guessed, word):
-    for char in word:
-        if char not in correct_chars_guessed:
-            return False
-    return True
-
-# Function to check status of game
-# Returns two element array:
-## First Element tells program if game reached a end state.
-## Second Element is either False (player lost), True (player won), or None (Game is not over yet).
-def is_game_over(num_of_wrong_guess, correct_chars_guessed, word):
-    if num_of_wrong_guess >= 6:
-        return [True, False]
-    elif all_chars_found(correct_chars_guessed, word):
-        return [True, True]
-    else:
-        return [False, None]
+    def update_chars_positions_dict(self):
+        for index, char in enumerate(self.current_word):
+            if char not in self.chars_positions_in_word_dict:
+                self.chars_positions_in_word_dict[char] = []
+            self.chars_positions_in_word_dict[char].append(index)
     
-
-def showcase_wrong_guesses(incorrect_chars_guessed):
-    print("Wrong guesses: ", end="")
-    for char in incorrect_chars_guessed:
-        print(char+" ", end="")
-    print()
-
-def update_player_progress(correct_chars_guessed, word):
-    return_str = "  "
-    if len(correct_chars_guessed) == 0:
-        for char in word:
-            return_str += "_  "
-    else:
-        for char in word:
-            if char in correct_chars_guessed:
-                return_str += char + "  "
-            else:
-                return_str += "_  "
-    return return_str
- 
-
-## Need to finish.
-def game_loop(word):
-    num_of_wrong_guesses = 0
-    chances_left = 6
-    correct_chars_guessed = []
-    incorrect_chars_guessed = []
-    game_is_over = None
-    player_progress = update_player_progress(correct_chars_guessed, word)
-    while True:
-        clear_terminal()
-        print(player_progress)
-        game_is_over = is_game_over(num_of_wrong_guesses, correct_chars_guessed, word)
-        if game_is_over[0]:
-            if game_is_over[1]:
-                print("You've won!")
-            else:
-                print("You've lost :(")
-                print("The word was " + word + "!")
-            exit()
-        print("Chances left: " + str(chances_left))
-        showcase_wrong_guesses(incorrect_chars_guessed)
-        user_guess = input("Guess: ")
-        if(user_guess.lower() == 'exit'):
-            exit()
-        if not re.match("^[A-Za-z]{1}$", user_guess):
-            print("Please limit the input to only one alphabetic character.")
-            continue
-        if user_guess in word:
-            correct_chars_guessed.append(user_guess)
+    def update_current_word_progress(self):
+        current_char_positions_array = None
+        if self.current_word_progress == []:
+            for char in self.current_word:
+                self.current_word_progress.append(" ")
         else:
-            if user_guess not in incorrect_chars_guessed:
-                incorrect_chars_guessed.append(user_guess)
-                num_of_wrong_guesses += 1
-                chances_left -= 1
+            for char in self.correct_char_guesses:
+                current_char_positions_array = self.chars_positions_in_word_dict[char]
+                for position in current_char_positions_array:
+                    self.current_word_progress[position] = char
+
+    def all_chars_found(self):
+        for char in self.current_word:
+            if char not in self.correct_char_guesses:
+                return False
+        return True
+
+    # Function to check status of game
+    # Returns two element array:
+    ## First Element tells program if game reached a end state.
+    ## Second Element is either False (player lost), True (player won), or None (Game is not over yet).
+    def check_game_status(self):
+        if self.number_of_wrong_guesses >= 6:
+            return [True, False]
+        elif self.all_chars_found():
+            return [True, True]
+        else:
+            return [False, None]
+        
+    def process_guess(self, input):
+        return_value = None
+        if input.upper() in self.get_current_word():
+            self.correct_char_guesses.append(input.upper())
+            self.update_current_word_progress()
+            return_value = True
+        else:
+            self.number_of_wrong_guesses += 1
+            if input.upper() not in self.incorrect_char_guesses:
+                self.incorrect_char_guesses.append(input.upper())
+            return_value = False
+
+        self.is_the_game_over = self.check_game_status()[0]
+        if self.is_the_game_over:
+            self.did_you_win = self.check_game_status()[1]
+        return return_value
+            
+
+
+    def reset_hangman(self):
+        self.number_of_wrong_guesses = 0
+        self.is_the_game_over = False
+        self.did_you_win = None
+        self.current_word = None
+        self.correct_char_guesses = []
+        self.incorrect_char_guesses = []
+        self.current_word_progress = []
+        self.chars_positions_in_word_dict = {}
+
+    def game_loop(word):
+        num_of_wrong_guesses = 0
+        chances_left = 6
+        correct_chars_guessed = []
+        incorrect_chars_guessed = []
+        game_is_over = None
         player_progress = update_player_progress(correct_chars_guessed, word)
+        while True:
+            print(player_progress)
+            game_is_over = is_game_over(num_of_wrong_guesses, correct_chars_guessed, word)
+            if game_is_over[0]:
+                if game_is_over[1]:
+                    print("You've won!")
+                else:
+                    print("You've lost :(")
+                    print("The word was " + word + "!")
+                exit()
+            print("Chances left: " + str(chances_left))
+            showcase_wrong_guesses(incorrect_chars_guessed)
+            user_guess = input("Guess: ")
+            if(user_guess.lower() == 'exit'):
+                exit()
+            if not re.match("^[A-Za-z]{1}$", user_guess):
+                print("Please limit the input to only one alphabetic character.")
+                continue
+            if user_guess in word:
+                correct_chars_guessed.append(user_guess)
+            else:
+                if user_guess not in incorrect_chars_guessed:
+                    incorrect_chars_guessed.append(user_guess)
+                    num_of_wrong_guesses += 1
+                    chances_left -= 1
+            player_progress = update_player_progress(correct_chars_guessed, word)
 
-def game():
-    clear_terminal()
-    difficulty = difficulty_prompt()
-    word = select_word(difficulty)
-    game_loop(word)
+
+
+
 
 # game()
