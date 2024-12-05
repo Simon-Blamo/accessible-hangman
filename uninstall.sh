a8b8eb7abe936952905d3e2260825ca774f85bbb
Simon Blamo <simonblamo.dev@proton.me>
2024-12-04 21:51:19 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,11 @@
-platform_name=$(python mac_machine_checker.py)
+platform_name=$(python ./src/mac_machine_checker.py)
 if [ "$platform_name" == "win32" ]; then
   source "C:/ProgramData/anaconda3/etc/profile.d/conda.sh"
 fi
+platform_name=$(py ./src/mac_machine_checker.py)
+if [ "$platform_name" == "win32" ]; then
+  source "C:/ProgramData/anaconda3/etc/profile.d/conda.sh"
+fi
+
 conda deactivate
 conda remove -n FINAL --all
