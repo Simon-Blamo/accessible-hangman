2f107f06336b5e49bc93f1f4c2f72b2f330d375a
Simon Blamo <simonblamo.dev@proton.me>
2024-12-04 21:28:09 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,3 @@ if [ "$platform_name" == "win32" ]; then
     source "C:/ProgramData/anaconda3/etc/profile.d/conda.sh"
 fi
 conda create -n FINAL python=3.9
-conda activate FINAL
-
-pip install pyttsx3
-pip install SpeechRecognition
-
-echo $platform_name
-if [ "$platform_name" == "darwin" ]; then
-    brew install portaudio
-fi
-pip install pyaudio
-pip install PyQt6
-pip install nltk
