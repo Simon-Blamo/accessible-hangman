4705123d0d60a7f97524736baacea9c4e20082e5
Simon Blamo <simonblamo.dev@proton.me>
2024-12-04 21:09:09 -0500

    Update conda_dependencies.sh

--- conda_dependencies.sh
+++ conda_dependencies.sh
@@ -1,1 +1,17 @@
-a
+conda activate FINAL
+
+# Install necessary dependencies
+pip install pyttsx3
+pip install SpeechRecognition
+
+echo $platform_name
+
+# Platform-specific steps for macOS
+if [ "$platform_name" == "darwin" ]; then
+    brew install portaudio
+fi
+
+# Install additional packages
+pip install pyaudio
+pip install PyQt6
+pip install nltk
