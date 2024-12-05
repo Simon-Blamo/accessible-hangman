58d2f78718b1aed613735047f99f5d334e9dfc31
Simon Blamo <simonblamo.dev@proton.me>
2024-12-04 21:20:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,17 @@
 platform_name=$(python mac_machine_checker.py)
-
-# Ensure conda is initialized correctly for different platforms
 if [ "$platform_name" == "win32" ]; then
     source "C:/ProgramData/anaconda3/etc/profile.d/conda.sh"
-elif [ "$platform_name" == "darwin" ]; then
-    # Ensure conda is initialized for macOS
-    source "$(conda info --base)/etc/profile.d/conda.sh"
 fi
+conda create -n FINAL python=3.9
+conda activate FINAL
 
+pip install pyttsx3
+pip install SpeechRecognition
 
-# Create the environment and activate it
-conda create -n FINAL python=3.9 -y
-
-# Use conda activate in the same shell session
-# conda activate FINAL
-
-# # Install necessary dependencies
-# pip install pyttsx3
-# pip install SpeechRecognition
-
-# echo $platform_name
-
-# # Platform-specific steps for macOS
-# if [ "$platform_name" == "darwin" ]; then
-#     brew install portaudio
-# fi
-
-# # Install additional packages
-# pip install pyaudio
-# pip install PyQt6
-# pip install nltk
+echo $platform_name
+if [ "$platform_name" == "darwin" ]; then
+    brew install portaudio
+fi
+pip install pyaudio
+pip install PyQt6
+pip install nltk
