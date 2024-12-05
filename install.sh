1a49abe21db5709f3a83f10b422151eecaa2f5be
Simon Blamo <simonblamo.dev@proton.me>
2024-12-04 21:53:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,12 @@ platform_name=$(python ./src/mac_machine_checker.py)
 if [ "$platform_name" == "win32" ]; then
     source "C:/ProgramData/anaconda3/etc/profile.d/conda.sh"
 fi
+platform_name=$(py ./src/mac_machine_checker.py)
+if [ "$platform_name" == "win32" ]; then
+    source "C:/ProgramData/anaconda3/etc/profile.d/conda.sh"
+fi
+platform_name=$(python3 ./src/mac_machine_checker.py)
+if [ "$platform_name" == "win32" ]; then
+    source "C:/ProgramData/anaconda3/etc/profile.d/conda.sh"
+fi
 conda create -n FINAL python=3.9
