e36a2ac58285ef802c830ab29467ea51c7c9a133
Simon Blamo <simonblamo.dev@proton.me>
2024-12-04 21:46:26 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 platform_name=$(python mac_machine_checker.py)
 if [ "$platform_name" == "win32" ]; then
     source "C:/ProgramData/anaconda3/etc/profile.d/conda.sh"
-elif [ "$platform_name" == "darwin" ]; then
-    # Ensure conda is initialized for macOS
-    source "$(conda info --base)/etc/profile.d/conda.sh"
 fi
 conda create -n FINAL python=3.9
