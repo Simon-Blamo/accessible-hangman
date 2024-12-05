d3e01eacf1289459399481b6b3e3a627bdba580d
Simon Blamo <simonblamo.dev@proton.me>
2024-12-04 21:47:32 -0500

    Update conda_dependencies.sh

--- conda_dependencies.sh
+++ conda_dependencies.sh
@@ -1,13 +1,20 @@
+# Platform-specific steps for macOS
 platform_name=$(python mac_machine_checker.py)
+if [ "$platform_name" == "darwin" ]; then
+    brew install portaudio
+fi
+platform_name=$(py mac_machine_checker.py)
+if [ "$platform_name" == "darwin" ]; then
+    brew install portaudio
+fi
+platform_name=$(python3 mac_machine_checker.py)
+if [ "$platform_name" == "darwin" ]; then
+    brew install portaudio
+fi
 
 # Install necessary dependencies
 pip install pyttsx3
 pip install SpeechRecognition
-
-# Platform-specific steps for macOS
-if [ "$platform_name" == "darwin" ]; then
-    brew install portaudio
-fi
 
 # Install additional packages
 pip install pyaudio
