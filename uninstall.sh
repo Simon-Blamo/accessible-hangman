907b2c5243ef34f1733c84c8141e07afe8e18c22
Simon Blamo <simonblamo.dev@proton.me>
2024-12-04 21:53:37 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,11 @@ platform_name=$(python ./src/mac_machine_checker.py)
 if [ "$platform_name" == "win32" ]; then
   source "C:/ProgramData/anaconda3/etc/profile.d/conda.sh"
 fi
+platform_name=$(python3 ./src/mac_machine_checker.py)
+if [ "$platform_name" == "win32" ]; then
+  source "C:/ProgramData/anaconda3/etc/profile.d/conda.sh"
+fi
+
 platform_name=$(py ./src/mac_machine_checker.py)
 if [ "$platform_name" == "win32" ]; then
   source "C:/ProgramData/anaconda3/etc/profile.d/conda.sh"
